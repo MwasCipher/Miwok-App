59c49b01d7aac02c0ea73361b3c8f384ff72490e
MwasCipher <mwangicollinswaweru@gmail.com>
2019-12-31 15:47:52 +0300

    Setting Visibility For Views

--- app/src/main/java/com/example/miwokapp/Word.java
+++ app/src/main/java/com/example/miwokapp/Word.java
@@ -3,9 +3,18 @@ package com.example.miwokapp;
 public class Word {
 
     private String englishWord, miwokWord;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
+
+    public static final int NO_IMAGE_PROVIDED = -1;
 
     public Word() {
 
+    }
+
+    public Word(String englishWord, String miwokWord, int imageResourceId) {
+        this.englishWord = englishWord;
+        this.miwokWord = miwokWord;
+        this.imageResourceId = imageResourceId;
     }
 
     public Word(String englishWord, String miwokWord) {
@@ -17,15 +26,15 @@ public class Word {
         return englishWord;
     }
 
-    public void setEnglishWord(String englishWord) {
-        this.englishWord = englishWord;
-    }
-
     public String getMiwokWord() {
         return miwokWord;
     }
 
-    public void setMiwokWord(String miwokWord) {
-        this.miwokWord = miwokWord;
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public boolean hasImage(){
+        return imageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/miwokapp/WordAdapter.java
+++ app/src/main/java/com/example/miwokapp/WordAdapter.java
@@ -5,13 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
@@ -32,9 +32,17 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView defaultWord = listViewItem.findViewById(R.id.english_word_text_view);
         TextView miwokWord = listViewItem.findViewById(R.id.miwok_word_text_view);
+        ImageView imageView = listViewItem.findViewById(R.id.word_image);
 
         defaultWord.setText(currentWord.getEnglishWord());
         miwokWord.setText(currentWord.getMiwokWord());
+
+        if (currentWord.hasImage()){
+            imageView.setImageResource(currentWord.getImageResourceId());
+        }else {
+            imageView.setVisibility(View.GONE);
+        }
+
 
         return listViewItem;
     }
