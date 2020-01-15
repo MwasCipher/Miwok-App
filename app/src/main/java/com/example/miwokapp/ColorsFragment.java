d4040d59512baf2783f828c4db08e860c0abe9fd
MwasCipher <mwangicollinswaweru@gmail.com>
2020-01-15 13:20:22 +0300

    Fragments

---
+++ app/src/main/java/com/example/miwokapp/ColorsFragment.java
@@ -0,0 +1,173 @@
+package com.example.miwokapp;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ColorsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ColorsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ColorsFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+    MediaPlayer mediaPlayer;
+
+    public ColorsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ColorsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ColorsFragment newInstance(String param1, String param2) {
+        ColorsFragment fragment = new ColorsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+
+
+            // Create a list of words
+            final ArrayList<Word> words = new ArrayList<Word>();
+            words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red,R.raw.color_red));
+            words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow,R.raw.color_mustard_yellow));
+            words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow,R.raw.color_dusty_yellow));
+            words.add(new Word("green", "chokokki", R.drawable.color_green,R.raw.color_green));
+            words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown,R.raw.color_brown));
+            words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray,R.raw.color_gray));
+            words.add(new Word("black", "kululli", R.drawable.color_black,R.raw.color_black));
+            words.add(new Word("white", "kelelli", R.drawable.color_white,R.raw.color_white));
+
+            // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+            // adapter knows how to create list items for each item in the list.
+            WordAdapter adapter = new WordAdapter(getContext(), R.color.category_colors, words);
+
+            // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+            // There should be a {@link ListView} with the view ID called list, which is declared in the
+            // word_list.xml layout file.
+            ListView listView = findViewById(R.id.list);
+
+            // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+            // {@link ListView} will display list items for each {@link Word} in the list.
+            listView.setAdapter(adapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Word word = words.get(position);
+                    releaseMediaPlayer();
+                    mediaPlayer = MediaPlayer.create(getContext(), word.getAudioResourceId());
+                    mediaPlayer.start();
+
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            releaseMediaPlayer();
+                        }
+                    });
+
+                }
+            });
+
+    }
+
+    private void releaseMediaPlayer(){
+        if (mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_colors, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
