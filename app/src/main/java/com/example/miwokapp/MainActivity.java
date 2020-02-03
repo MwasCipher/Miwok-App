a088a48d3176d646f5693d00f8d5004c89a43509
MwasCipher <mwangicollinswaweru@gmail.com>
2020-02-03 17:36:04 +0300

    View Pager

--- app/src/main/java/com/example/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/miwokapp/MainActivity.java
@@ -1,21 +1,19 @@
 package com.example.miwokapp;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView numbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewpager);
+        ViewPager viewPager = findViewById(R.id.view_pager);
 
         CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
